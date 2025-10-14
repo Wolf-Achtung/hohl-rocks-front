d019991236acceb4ae2d75a931cc1960a5e6c26e
Wolf-Achtung <wolf.hohl@web.de>
2025-10-14 19:11:40 +0200

    Delete playwright.config.ts

--- playwright.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig, devices } from '@playwright/test';
-export default defineConfig({
-  testDir: './tests',
-  use: { baseURL: 'http://localhost:' + (process.env.PORT || 8080) },
-  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
-});
